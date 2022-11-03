0e8c6d80286e082f706b3f2e51d80dcd775d0e4b
Miriam <113472184+mmdezr@users.noreply.github.com>
2022-11-03 22:31:51 +0000

    Delete download/mi_string2 directory

--- download/mi_string2/mi_string.h
+++
@@ -1,7 +0,0 @@
-int mi_strlen (char* str);
-char* mi_strcpy (char* s1, char* s2);
-char* mi_strdup (char* str);
-char* mi_strcat (char* s1, char* s2);
-int mi_strequals (char* s1, char* s2);
-int testeador();
-
